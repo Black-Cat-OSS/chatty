4411e6c4d778ccc46eeed2e10f86cc6a5371351e
letnull19A <alexvolkovdd@googlemail.com>
2025-11-19 03:56:14 +0400

    refactor: enhance code formatting and consistency across frontend and backend files

--- server/dist/chat/chat.gateway.js
+++ server/dist/chat/chat.gateway.js
@@ -8,6 +8,7 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
+var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ChatGateway = void 0;
 const websockets_1 = require("@nestjs/websockets");
@@ -59,6 +60,6 @@ exports.ChatGateway = ChatGateway = __decorate([
             origin: '*',
         },
     }),
-    __metadata("design:paramtypes", [nestjs_pino_1.Logger])
+    __metadata("design:paramtypes", [typeof (_a = typeof nestjs_pino_1.Logger !== "undefined" && nestjs_pino_1.Logger) === "function" ? _a : Object])
 ], ChatGateway);
 //# sourceMappingURL=chat.gateway.js.map
