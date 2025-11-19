78010b4e31f6c3a64616e9e01de254848acaa8a9
letnull19A <alexvolkovdd@googlemail.com>
2025-11-19 04:05:13 +0400

    feat: implement global database module with dynamic connection using drizzle-orm and PostgreSQL

--- server/src/database/database.module.ts
+++ server/src/database/database.module.ts
@@ -1,8 +1,33 @@
-import { Module } from '@nestjs/common'
-import { databaseProviders } from './database.providers'
+import { Module, Global } from '@nestjs/common';
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { Pool } from 'pg';
+import { ConfigModule } from '../config/config.module';
+import { ConfigService } from '../config/config.service';
+import * as schema from './schema';
 
+export const DATABASE_CONNECTION = 'DATABASE_CONNECTION';
+
+@Global()
 @Module({
-  providers: [...databaseProviders],
-  exports: [...databaseProviders],
+  imports: [ConfigModule],
+  providers: [
+    {
+      provide: DATABASE_CONNECTION,
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        const dbConfig = configService.database;
+        const pool = new Pool({
+          host: dbConfig.host,
+          port: dbConfig.port,
+          user: dbConfig.user,
+          password: dbConfig.password,
+          database: dbConfig.database,
+        });
+
+        return drizzle(pool, { schema });
+      },
+    },
+  ],
+  exports: [DATABASE_CONNECTION],
 })
 export class DatabaseModule {}
